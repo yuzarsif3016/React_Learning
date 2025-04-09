463b832aad3405c30d85b50d20fac7c4ba36f852
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-04-09 19:44:49 +0530

    second episode

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement("h1",{id:"heading"}, "Hello world from React 😍😍");
 
 
@@ -5,9 +8,3 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 
 root.render(heading);
-
-<div>
-    <h1>
-        <h2></h2>
-    </h1>
-</div>

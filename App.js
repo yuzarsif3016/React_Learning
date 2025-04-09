dfc0a0e17fbfd052d08e35b698bf1d6b56c67339
yuzarsif3016 <yusufpathan3016@gmail.com>
2025-04-09 12:32:51 +0530

    episode 1

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1",{id:"heading"}, "Hello world from React 😍😍");
+
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+root.render(heading);
+
+<div>
+    <h1>
+        <h2></h2>
+    </h1>
+</div>
